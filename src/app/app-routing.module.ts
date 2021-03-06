2c5fdb093a14e09339bca6d18236ab08dcd28f23
mih777777 <mih777777@yandex.ru>
2021-03-06 13:05:40 +0300

    изменил роутинг и главный компонент

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: MainComponent },
+  { path: 'create', component: CreateComponent },
+  { path: 'update/:id', component: UpdateComponent },
+  { path: 'single-todo/:id', component: SingleTodoComponent },
+  { path: 'delete-category', component: DeleteCategoryComponent }
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

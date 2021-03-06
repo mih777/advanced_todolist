e9e6a5584a11a76a1964404c8f6afabb6642ced8
mih777777 <mih777777@yandex.ru>
2021-03-06 13:23:10 +0300

    все фурычит но стили не все

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateComponent } from './components/create/create.component';
+import { DeleteCategoryComponent } from './components/delete-category/delete-category.component';
+import { MainComponent } from './components/main/main.component';
+import { SingleTodoComponent } from './components/single-todo/single-todo.component';
+import { UpdateComponent } from './components/update/update.component';
 
 const routes: Routes = [
   { path: '', component: MainComponent },

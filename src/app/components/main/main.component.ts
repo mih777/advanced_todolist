61b196cb4428bb9492b39293ff6896054131e11f
mih777 <mih777777@yandex.ru>
2021-03-29 22:35:14 +0300

    change true to false

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -9,7 +9,7 @@ import { Router } from '@angular/router';
 })
 export class MainComponent implements OnInit {
 
-  togle = true
+  togle = false
 
   todos: Todo[] = []
   categories: Category[] = []
